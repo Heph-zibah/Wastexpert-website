e838eeebc30f5340a3c6c9eeb61de286769134d1
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-10-07 20:30:55 +0100

    update file

---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import heroImg from '../Assets/HomePage/hero-img.svg';
+
+const Hero = () => {
+	return (
+		<div>
+			<div className=''>
+				<img src={heroImg} alt='' />
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		screens: {
-			sm: '375px',
+			sm: '428px',
 			md: '768px',
 			lg: '1020px',
 			xl: '1440px'
@@ -12,6 +12,10 @@ module.exports = {
 			colors: {
 				primary: '#028C32',
 				secondary: 'rgba(0, 0, 0, 0.5)'
+			},
+			fontFamily: {
+				inter: ['Inter', 'sans - serif'],
+				mulish: ['Mulish', 'sans - serif']
 			}
 		}
 	},
