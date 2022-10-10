8c95ae9f6ecde373b17bae396df43e126ba9b6c5
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-10-10 08:00:23 +0100

    finished the responsiveness

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -11,20 +11,21 @@ const About = () => {
 	return (
 		<div>
 			<Navbar />
+			{/*HERO SECTION */}
 			<div>
 				<div className='flex flex-col lg:flex-row-reverse'>
 					<div className=''>
 						<img src={aboutHero} alt='three people' />
 					</div>
 					<div className='bg-primary w-full h-[39px] lg:hidden'></div>
-					<div className='px-[19px] pt-[29px] text-justify flex flex-col items-center lg:w-1/2 lg:items-start lg:px-0 lg:pl-[70px] xl:pl-[113px] lg:pt-[62px]'>
+					<div className='px-[19px] pt-[29px] md:px-[60px] text-justify flex flex-col items-center lg:w-1/2 lg:items-start lg:px-0 lg:pt-[32px] lg:pl-[70px] xl:pl-[85px] xxl:pl-[113px] xl:pt-[42px] xxl:pt-[62px]'>
 						<div>
 							<h1 className='font-inter text-primary font-bold text-[40px]'>
 								About Us
 							</h1>
 							<img src={aboutLine} alt='a divider' />
 						</div>
-						<p className='font-mulish text-[18px] text-[#666666] pt-[20px] lg:pt-[33px] lg:pr-[114px] lg:text-justify lg:leading-[140%] '>
+						<p className='font-mulish text-[18px] text-[#666666] pt-[20px] lg:pt-[33px] xl:pr-[14px]  xxl:pr-[114px] lg:text-justify xl:leading-[140%] '>
 							Wastexpert is a waste recycling and social benefit venture borne out of
 							sheer passion and vision to transform our environment into a zero
 							landfill and zero waste nation. We operates an incentive-based scheme
@@ -39,18 +40,19 @@ const About = () => {
 				</div>
 				<div className='bg-primary w-full h-[56px] hidden lg:block'></div>
 			</div>
+			{/*WASTE MENACE SECTION */}
 			<div className='flex flex-col lg:flex-row mt-20 lg:mt-0'>
-				<div>
+				<div className='lg:mt-[70px] xl:mt-0 '>
 					<img src={chinaWaste} alt='dirt' />
 				</div>
-				<div className='px-[19px] pt-[29px] text-center flex flex-col items-center lg:w-1/2 lg:items-start lg:text-start lg:pl-[34px] lg:pr-[142px] lg:pt-[61px]'>
+				<div className='px-[19px] pt-[29px] md:px-[60px] text-center flex flex-col items-center lg:w-1/2 lg:items-start lg:text-start lg:pl-[34px] lg:pr-[60px] xl:pr-[85px] xxl:pr-[142px] lg:pt-[61px] xl:pt-[21px] xxl:pt-[31px]'>
 					<div>
 						<h2 className='font-inter text-primary font-bold text-[40px]'>
 							Waste Menace
 						</h2>
 						<img src={aboutLine} alt='divider' />
 					</div>
-					<p className='font-mulish text-[18px] text-[#666666] pt-[20px] lg:pt-[33px] text-justify lg:leading-[140%] '>
+					<p className='font-mulish text-[18px] text-[#666666] pt-[20px] lg:pt-[33px] xl:pt-[23px] xxl:pt-[33px] text-justify lg:leading-[140%] '>
 						It is a common sight to see our streets, drainages, roadsides and even
 						dumpsites packed with recyclable packaging materials which are carelessly
 						disposed of by users. This practice often poses environmental hazards to
@@ -67,20 +69,29 @@ const About = () => {
 					</p>
 				</div>
 			</div>
-			<div className='flex flex-col lg:flex-row-reverse mt-[64px]'>
+			{/*SOLUTION SECTION */}
+			<div className='flex flex-col lg:flex-row-reverse mt-[64px] '>
 				<div className='flex about-bg '>
-					<img src={bottle} alt='bottles' className='mb-[-200px] ml-[-100px]' />
-					<img src={bin} alt='waste bin' className='ml-[-230px]' />
+					<img
+						src={bottle}
+						alt='bottles'
+						className='mb-[-150px] ml-[-50px] lg:mb-[-230px] xl:mb-[-240px] xxl:mb-[-200px] xl:ml-[-100px] w-[70%] xxl:w-[100%]'
+					/>
+					<img
+						src={bin}
+						alt='waste bin'
+						className='ml-[-130px] md:ml-[-200px] xl:ml-[-210px] xxl:ml-[-230px] w-[70%] xxl:w-[100%]'
+					/>
 				</div>
 
-				<div className='w-1/2 lg:pl-[70px] xl:pl-[113px]'>
+				<div className='lg:w-1/2 mt-[40px] lg:mt-0 px-[19px] md:px-[60px] lg:pl-[70px] lg:pr-[10px] xl:pr-[100px]'>
 					<div>
 						<h3 className='font-inter text-primary font-bold text-[40px]'>
 							Our Solution
 						</h3>
 						<img src={aboutLine} alt='divider' />
 					</div>
-					<p className='font-mulish text-[18px] text-[#666666] pt-[20px] lg:pt-[33px] lg:pr-[14px] lg:text-justify lg:leading-[140%] lg:mt-[170px]'>
+					<p className='font-mulish text-[18px] text-[#666666] pt-[20px] lg:pt-[33px] lg:pr-[14px] text-justify lg:leading-[140%] lg:mt-[30px] xl:mt-[30px] xxl:mt-[100px]'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mollis leo auctor
 						a scelerisque vel sit cras eu consectetur. Viverra venenatis, mauris diam
 						dolor cras a. Lectus massa aenean neque pulvinar tortor, a. Nisl, dictumst
@@ -94,6 +105,14 @@ const About = () => {
 						tortor, a. Nisl, dictumst eu urna, at odio scelerisque pulvinar a, magna.{' '}
 					</p>
 				</div>
+			</div>
+			{/*QUOTE SECTION */}
+			<div className='px-[19px] py-[80px] md:px-[60px] lg:pb-[110px] lg:pt-[105px] xl:pb-[170px] xl:pt-[155px] text-center font-mulish quote lg:px-[172px] xl:px-[200px] xxl:px-[272px]'>
+				<p className='text-primary font-semibold  text-[30px]'>
+					If you care about the environment and how it will affect mankind, then
+					recycling shoudn’t take a second thought.
+				</p>
+				<p className='text-[18px]'>Jeffery Sanderson</p>
 			</div>
 			<Footer />
 		</div>
