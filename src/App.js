718e328bcc3b2c4040ceb74ebab7d1f8ccca3ff0
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-10-10 12:44:40 +0100

    update file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Services from './Pages/Services';
 import Contact from './Pages/Contact';
 import Faq from './Pages/Faq';
+import Login from './Components/Login';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<Router>
 			<Routes>
 				<Route path='/' element={<Home />}></Route>
 				<Route path='/About' element={<About />}></Route>
 				<Route path='/Services' element={<Services />}></Route>
 				<Route path='/Contact' element={<Contact />}></Route>
 				<Route path='/Faq' element={<Faq />}></Route>
+				<Route path='/Login' element={<Login />}></Route>
 			</Routes>
-		</BrowserRouter>
+		</Router>
 	);
 }
 
